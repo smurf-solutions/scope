9efe335fc1bfa12e94f2f5fbf9d4afc23f8ce1d7
S. Kotarov <skotarov@gmail.com>
2017-12-05 18:23:57 +0200

    Update scope1.js

--- scope1.js
+++ scope1.js
@@ -409,6 +409,7 @@ let scopeTemplate = {
 
 			if(el.dataset.json){
 				el.originalJson = el.dataset.json 
+				$data = el.parent.data||{}
 				try{ eval("el.data="+el.dataset.json)
 				}catch(e){ console.error("Parsing 'data-json' => ",e.toString(),"\n\n-- data\n",el.dataset.json,"\n\n-- element",el); return }
 			} else el.data = {}
